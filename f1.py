f2b3efb3594b4ff30b35c92d6fa495697716030a
N.Nandan kumar <nandunandan593@gmail.com>
2025-06-26 11:35:15 +0530

    add the file

---
+++ f1.py
@@ -0,0 +1,6 @@
+def add1(a, b):
+    print("The sum is:", a + b)
+
+a = int(input())
+b = int(input())
+add1(a, b)
---
+++ nest.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a number: "))
+if n>90:
+    if 100<n<90:
+        print("grade A")
+    elif 80<n<=90:
+        print("grade B")
+        
